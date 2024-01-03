e6c3c0a846ad867452fe09a3b0ff54858eac6b76
Conor Redington <conorredington@gmail.com>
2024-01-03 20:35:40 +0000

    Implement way to show png of created graph

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ The goal is to construct a “model of a system about which we would like to rea
 More generally, just looking at declarative representations of a model so that the reasoning process is
 independent of the representation.
 
-# TODO 
 
-- [] What are the types of interest (using Koller and Friedman)?
-- [] Investigate a graphing library.
+## Bayesian Networks
+
+A graph, where the nodes are random variables. Each random variable has a domain and range. The space of output values
+of the random variable has an associated probability defined 
+
+## Types
+
+- Bayesian Network is a pair (G, P) where P _factorizes_ over G.
+- P is a set of CPD's (Conditional Probability Distributions) associated with the nodes in G.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
-mod graph;
-mod variable;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::DiGraph;
+use std::fmt;
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Debug, Default)]
+struct DiscreteRandomVariable {
+    name: String,
+    domain: Vec<f32>,
+}
+
+impl fmt::Display for DiscreteRandomVariable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl DiscreteRandomVariable {
+    fn new(name: String, domain: Vec<f32>) -> Self {
+        DiscreteRandomVariable { name, domain }
+    }
+}
 
 fn main() {
-    let v1 = variable::Variable::new(0.5);
-    let g = graph::Graph::new(vec![v1]);
-    dbg!(g);
+    let mut bg = DiGraph::<DiscreteRandomVariable, i32>::new();
+
+    let d = bg.add_node(DiscreteRandomVariable::new(
+        String::from("D"),
+        vec![0.6, 0.4],
+    ));
+    let i = bg.add_node(DiscreteRandomVariable::new(
+        String::from("I"),
+        vec![0.7, 0.3],
+    ));
+    let g = bg.add_node(DiscreteRandomVariable::new(
+        String::from("G"),
+        vec![0.0, 0.0, 0.0],
+    ));
+    bg.add_edge(d, g, 1);
+    bg.add_edge(i, g, 2);
+
+    println!("{:?}", Dot::with_config(&bg, &[Config::EdgeNoLabel]));
+    let mut f = File::create("img/bg.dot").unwrap();
+    let output = format!("{}", Dot::with_config(&bg, &[Config::EdgeNoLabel]));
+    f.write_all(&output.as_bytes())
+        .expect("could not write file");
 }

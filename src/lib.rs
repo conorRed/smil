96434d6df08aecbee9566a81482ac7e34a41ddef
Conor Redington <conor@AER-OXF-TEC-CRE>
2024-01-11 17:17:05 +0000

    It was the cartesian product

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 
 // how do we limit the arguments to factor? Is it a simple function?
 
+use itertools::Itertools;
 use petgraph::graph::DiGraph;
 use std::fmt;
 
@@ -11,7 +12,7 @@ use std::fmt;
 #[derive(Debug, Clone)]
 pub struct DiscreteRandomVariable {
     name: String,
-    domain: Vec<f32>,
+    domain: Vec<i32>,
     cardinality: i32,
 }
 
@@ -26,7 +27,7 @@ impl fmt::Display for DiscreteRandomVariable {
 }
 
 impl DiscreteRandomVariable {
-    pub fn new(name: String, domain: Vec<f32>, cardinality: i32) -> Self {
+    pub fn new(name: String, domain: Vec<i32>, cardinality: i32) -> Self {
         DiscreteRandomVariable {
             name,
             domain,
@@ -37,57 +38,43 @@ impl DiscreteRandomVariable {
     //    In general, each variable X in the model is associated with a conditional probability
     //    distribution (CPD) that specifies a distribution CPD over the values of X given each possible
     //    joint assignment of values to its parents in the model.
-    pub fn cpd(&self, conditionals: &Vec<DiscreteRandomVariable>) -> DiGraph<f32, &str> {
-        // create a graph of the combinatorial space.
-        let mut combinatorial_space = DiGraph::<f32, &str>::new();
-        // a node for each combination
-        // a sub tree for each domain value.
-        for &d in self.domain.iter() {
-            // create left
-            // create right
-            let root = combinatorial_space.add_node(d);
-            // add child nodes of all conditionals
-            for rv in conditionals {
-                for &cd in rv.domain.iter() {
-                    let node_cd = combinatorial_space.add_node(cd);
-                    combinatorial_space.add_edge(node_d, node_cd, "");
-                }
-            }
-        }
+    pub fn cpd(&self, conditionals: &Vec<DiscreteRandomVariable>) -> () {
+        let mut vec_of_domains: Vec<Vec<i32>> =
+            conditionals.iter().map(|rv| rv.domain.clone()).collect();
 
-        return combinatorial_space;
-    }
+        vec_of_domains.push(self.domain.clone());
+        let _outcome_space_size: usize =
+            conditionals.iter().fold(1, |acc, rv| rv.domain.len() * acc) * self.domain.len();
 
-    pub fn subtree_(root_value: f32, children: &Vec<f32>) -> DiGraph<f32, &str> {
-        let mut subtree: DiGraph<f32, &str> = DiGraph::<f32, &str>::new();
-        let root = subtree.add_node(root_value);
-        if children.len() == 0 {
-            return subtree;
-        }
-        for child in children {
-            subtree.add_node(subtree_(child, ));
-        }
-        return subtree;
+        let cartesian_product: Vec<Vec<&i32>> = vec_of_domains
+            .iter()
+            .multi_cartesian_product()
+            .take(_outcome_space_size)
+            .collect();
+        println!(
+            "Outcome space: {} {:?}",
+            _outcome_space_size, cartesian_product
+        );
     }
 
     // return sub graph for one combination
     // The Garden of Forking paths!
-    pub fn inner_cpd(&self, domain_value: f32, conditionals: &Vec<f32>) -> DiGraph<f32, &str> {
-        let mut combinatorial_space = DiGraph::<f32, &str>::new();
-        let node_d = combinatorial_space.add_node(domain_value);
-        if conditionals.len() == 0 {
-            return combinatorial_space;
-        }
-        for &cd in conditionals {
-            let node_cd = cd.cpd(cd, &conditionals[1..]);
-            inner_cpd(cd, )
-            combinatorial_space.add_edge(node_d, node_cd, "");
-        }
-
-        return combinatorial_space;
-    }
 }
 
+pub fn assignments(conditionals: &Vec<DiscreteRandomVariable>) -> () {
+    let vec_of_domains: Vec<Vec<i32>> = conditionals.iter().map(|rv| rv.domain.clone()).collect();
+    let _outcome_space_size: usize = conditionals.iter().fold(1, |acc, rv| rv.domain.len() * acc);
+
+    let cartesian_product: Vec<Vec<&i32>> = vec_of_domains
+        .iter()
+        .multi_cartesian_product()
+        .take(_outcome_space_size)
+        .collect();
+    println!(
+        "Outcome space: {} {:?}",
+        _outcome_space_size, cartesian_product
+    );
+}
 // Need to think about a state space. What is the 'combinatorial expolsian' of the random variables
 // and do we have a factor across any particular point in this space
 // JointDistribution is a type for this.
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
 mod lib;
+use lib::assignments;
 use lib::DiscreteRandomVariable;
 use petgraph::dot::{Config, Dot};
 use petgraph::graph::DiGraph;
 use std::fs::File;
 use std::io::Write;
 
-fn main() -> std::io::Result<()> {
-    let d = DiscreteRandomVariable::new(String::from("D"), vec![0.6, 0.4], 2);
-    let i = DiscreteRandomVariable::new(String::from("I"), vec![0.7, 0.3], 2);
-    let g = DiscreteRandomVariable::new(String::from("G"), vec![0.1, 0.2, 0.3], 2);
-    let combinations_g = g.cpd(&vec![d, i]);
-    return save_graph_f32(&combinations_g, "img/combinations.dot");
-}
-
-fn example() -> std::io::Result<()> {
-    let mut bg = DiGraph::<DiscreteRandomVariable, &str>::new();
-
-    let d = bg.add_node(DiscreteRandomVariable::new(
-        String::from("D"),
-        vec![0.6, 0.4],
-        2,
-    ));
-    let i = bg.add_node(DiscreteRandomVariable::new(
-        String::from("I"),
-        vec![0.7, 0.3],
-        2,
-    ));
-    let g = bg.add_node(DiscreteRandomVariable::new(
-        String::from("G"),
-        vec![0.0, 0.0, 0.0],
-        2,
-    ));
-    bg.add_edge(d, g, "");
-    bg.add_edge(i, g, "");
-    return save_graph(&bg, "img/bg.dot");
+fn main() -> () {
+    let d = DiscreteRandomVariable::new(String::from("D"), vec![6, 4], 2);
+    let i = DiscreteRandomVariable::new(String::from("I"), vec![7, 3], 2);
+    let g = DiscreteRandomVariable::new(String::from("G"), vec![1, 2, 3], 2);
+    let _combinations_g = assignments(&vec![d, i, g]);
 }
 
 fn save_graph_f32(bg: &DiGraph<f32, &str>, filename: &str) -> std::io::Result<()> {
